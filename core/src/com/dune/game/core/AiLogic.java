b44c8bbf97bf8dc1807445156593cbf98a8f113c
Alexandr Rybinskov <rybinskov@gmail.com>
2020-06-06 15:51:44 +0300

    last

--- core/src/com/dune/game/core/AiLogic.java
+++ core/src/com/dune/game/core/AiLogic.java
@@ -1,4 +1,47 @@
 package com.dune.game.core;
 
+import com.badlogic.gdx.math.Vector2;
+import com.dune.game.core.units.AbstractUnit;
+import com.dune.game.core.units.UnitType;
+
+import java.util.List;
+
 public class AiLogic {
+    private GameController gc;
+    private Vector2 temp;
+
+    public AiLogic(GameController gc) {
+        this.gc = gc;
+        temp = new Vector2();
+    }
+
+    public void update(float dt) {
+        if (!gc.getUnitsController().getAiUnits().isEmpty()) {
+            for (int i = 0; i < gc.getUnitsController().getAiUnits().size(); i++) {
+                AbstractUnit u = gc.getUnitsController().getAiUnits().get(i);
+                if ((u.getUnitType() == UnitType.HARVESTER)) {
+                    //Если в точке, где стоит танк, нет ресурсов, он едет их собирать туда, где они есть
+                    if (gc.getMap().getResourceCount(u.getPosition()) < 1) {
+                        u.commandMoveTo(collectResources());
+                    }
+                }
+            }
+        }
+    }
+
+    public void unitProcessing(AbstractUnit unit) {
+    }
+
+    public Vector2 collectResources() {
+        List<Vector2> resources = gc.getMap().getResourcesList();
+        if (!resources.isEmpty()) {
+            for (int i = 0; i < resources.size(); i++) {
+                if (gc.getMap().getResourceCount(resources.get(i)) > 0) {
+                    temp.set(resources.get(i));
+                } else continue;
+            }
+        }
+        return temp;
+    }
 }
+
--- core/src/com/dune/game/core/BattleMap.java
+++ core/src/com/dune/game/core/BattleMap.java
@@ -4,8 +4,9 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
-import com.dune.game.core.units.BattleTank;
-import com.dune.game.core.units.Harvester;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BattleMap {
     private class Cell {
@@ -57,12 +58,16 @@ public class BattleMap {
     public static final int COLUMNS_COUNT = 16;
     public static final int ROWS_COUNT = 9;
     public static final int CELL_SIZE = 80;
+    List<Vector2> resourcesList;
+    Vector2 temp;
 
     private TextureRegion grassTexture;
     private TextureRegion resourceTexture;
     private Cell[][] cells;
 
     public BattleMap() {
+        resourcesList = new ArrayList<>();
+        temp = new Vector2();
         this.grassTexture = Assets.getInstance().getAtlas().findRegion("grass");
         this.resourceTexture = Assets.getInstance().getAtlas().findRegion("resource");
         this.cells = new Cell[COLUMNS_COUNT][ROWS_COUNT];
@@ -71,12 +76,30 @@ public class BattleMap {
                 cells[i][j] = new Cell(i, j);
             }
         }
+        findResources();
+    }
+
+    public List<Vector2> getResourcesList() {
+        return resourcesList;
     }
 
     public int getResourceCount(Vector2 point) {
         int cx = (int) (point.x / CELL_SIZE);
         int cy = (int) (point.y / CELL_SIZE);
         return cells[cx][cy].resource;
+    }
+
+    public void findResources() {
+        //Харвестеры AI будут собирать ресурсы на своей половине карты
+        for (int i = 7; i < COLUMNS_COUNT; i++) {
+            for (int j = 0; j < ROWS_COUNT; j++) {
+                temp.set(i * 80, j * 80);
+                if ((cells[(int) temp.x / 80][(int) temp.y / 80].resourceRegenerationRate > 0)) {
+                    Vector2 rc = new Vector2(temp.x + 40.0f, temp.y + 40.0f);
+                    resourcesList.add(rc);
+                }
+            }
+        }
     }
 
     public int harvestResource(Vector2 point, int power) {
